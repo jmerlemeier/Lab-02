514c7468c6487545c6b308e94eba5f74fb39a2ed
Julie Erlemeier <jmerlemeier@gmail.com>
2019-07-09 15:41:52 -0700

    Added Questions 2 and 3 prompts

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 "use strict";
 
 //Question 1
-var harpAge = prompt("Welcome to julie e's 'About Me' page. There are 5 questions about julie for you to guess the answers to. First, Did you know Julie started playing the harp at 4 years old?");
+var harpAge = prompt("Welcome to Julie E's 'About Me' page. There are 5 questions about Julie for you to guess the answers to. First, Did you know Julie started playing the harp at 4-years-old?");
 
+//convert to lowercase
 harpAge.toLowerCase();
 
+//yes user input
 if(harpAge === 'yes' || harpAge === 'y') {
   alert('You did? You must know her very well.');
 } else {
@@ -12,14 +14,90 @@ if(harpAge === 'yes' || harpAge === 'y') {
   if(harpAge === 'YES' || harpAge === 'Y') {
     alert('You did? You must know her very well.');
   } else {
-    alert('Cool, well now you know.')
+    //nothing here
   }
 }
 
+// No user input
+if(harpAge === 'no' || harpAge === 'n') {
+  alert('You did not? That is ok. She was 4-years-old back in 1993.');
+} else {
+  harpAge.toUpperCase();
+  if(harpAge === 'NO' || harpAge === 'N') {
+    alert('You did not? That is ok. She was 4-years-old back in 1993.');
+  } else {
+    //nothing here
+  }
+}
+
+    //console log Q1
+    console.log('User\'s response to Question 1: ' + harpAge);
 
 //Question 2
+var grewupMinn = prompt("Julie grew up shoveling snow in the Great White North. Did you know that is one nickname for Minnesota?");
+
+//convert to lowercase
+grewupMinn.toLowerCase();
+
+//yes user input
+if(grewupMinn === 'yes' || grewupMinn === 'y') {
+  alert('Isn\'t Minnesota trivia the best? Another name for the state is the "Mini Soda.');
+} else {
+  grewupMinn.toUpperCase();
+  if(grewupMinn === 'YES' || grewupMinn === 'Y') {
+    alert('Isn\'t Minnesota trivia the best? Another name for the state is the "Mini Soda.');
+  } else {
+    //nothing here
+  }
+}
+
+// No user input
+if(grewupMinn === 'no' || grewupMinn === 'n') {
+  alert('No? Another name for the state is the "Mini Soda."');
+} else {
+  grewupMinn.toUpperCase();
+  if(grewupMinn === 'NO' || grewupMinn === 'N') {
+    alert('No? Another name for the state is the "Mini Soda."');
+  } else {
+    //nothing here
+  }
+}
+
+    //console log Q1
+    console.log('User\'s response to Question 2: ' + grewupMinn);
 
 //Question 3
+var bakingFun = prompt("Baking");
+
+//convert to lowercase
+bakingFun.toLowerCase();
+
+//yes user input
+if(bakingFun === 'yes' || bakingFun === 'y') {
+  alert('You did? You must know her very well.');
+} else {
+  bakingFun.toUpperCase();
+  if(bakingFun === 'YES' || bakingFun === 'Y') {
+    alert('You did? You must know her very well.');
+  } else {
+    //nothing here
+  }
+}
+
+// No user input
+if(bakingFun === 'no' || bakingFun === 'n') {
+  alert('You did not? That is ok. ');
+} else {
+  bakingFun.toUpperCase();
+  if(bakingFun === 'NO' || bakingFun === 'N') {
+    alert('You did not? That is ok.');
+  } else {
+    //nothing here
+  }
+}
+
+    //console log Q1
+    console.log('User\'s response to Question 3: ' + bakingFun);
 
 //Question 4
 
