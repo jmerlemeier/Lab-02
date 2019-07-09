26ecbb826e08c0dfeb34621bbc779b2c9cb916f6
Julie Erlemeier <jmerlemeier@gmail.com>
2019-07-09 14:55:49 -0700

    Created index with html and first prompt.

---
+++ js/app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+//Question 1
+var harpAge = prompt("Welcome to julie e's 'About Me' page. There are 5 questions about julie for you to guess the answers to. First, Did you know Julie started playing the harp at 4 years old?");
+
+harpAge.toLowerCase();
+
+if(harpAge === 'yes' || harpAge === 'y') {
+  alert('You did? You must know her very well.');
+} else {
+  harpAge.toUpperCase();
+  if(harpAge === 'YES' || harpAge === 'Y') {
+    alert('You did? You must know her very well.');
+  } else {
+    alert('Cool, well now you know.')
+  }
+}
+
+
+//Question 2
+
+//Question 3
+
+//Question 4
+
+//Question 5
+
+
